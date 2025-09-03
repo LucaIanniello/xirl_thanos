206017e81314e7908a240749a27be7920365a3ab
LazarVexx <luca.ianniello917@gmail.com>
2025-09-03 21:25:56 +0200

    update

--- rl_xmagical_learned_reward_multi.py
+++ rl_xmagical_learned_reward_multi.py
@@ -48,11 +48,10 @@ def run_seed(seed, kwargs, world_size, env_name, experiment_name):
     port = int(FLAGS.port_number) + (seed * 10)  # Unique port for each seed
     cmd = [
         "torchrun",
-        f"--nnodes=1",
-        f"--nproc-per-node={world_size}",
-        f"--rdzv-id={seed}",
-        f"--rdzv-backend=c10d",
-        f"--rdzv-endpoint=localhost:{port}",
+        f"--nproc_per_node={world_size}",
+        "--rdzv_backend=c10d",
+        f"--rdzv_endpoint=localhost:{port}",
+        "--max-restarts", "0",
         "train_policy_multi.py",
         "--experiment_name", f"{experiment_name}_seed{seed}",
         "--env_name", env_name,
