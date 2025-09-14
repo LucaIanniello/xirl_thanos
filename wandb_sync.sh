118b5b4fed4d83876584b1d6a15d8e1f7e35ed3c
LazarVexx <luca.ianniello917@gmail.com>
2025-09-14 10:09:44 +0200

    wandb sync file

---
+++ wandb_sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=wandb_sync
+#SBATCH --output=wandb_sync.log
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=100GB
+
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate xirl
+
+
+# Path to your zip
+ZIPFILE=/home/liannello/wandb_to_sync/all_runs.zip
+WORKDIR=$SLURM_TMPDIR/wandb_sync
+
+mkdir -p "$WORKDIR"
+cd "$WORKDIR"
+
+# Unzip the runs
+unzip "$ZIPFILE"
+
+# Loop through each run directory and sync
+for run_dir in offline-run-*; do
+    if [ -d "$run_dir" ]; then
+        echo ">>> Syncing $run_dir"
+        wandb sync "$run_dir"
+    fi
+done
+
+echo "All runs have been synced."
